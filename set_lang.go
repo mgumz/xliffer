0191192728c187c9fd022e05a131777bfda5ef23
Mathias Gumz <mgumz@tpip.net>
2015-10-02 13:59:37 +0200

    add new converter: 'set-lang'

---
+++ set_lang.go
@@ -0,0 +1,76 @@
+// This file is part of *xliffer*
+//
+// Copyright (C) 2015, Travelping GmbH <copyright@travelping.com>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+package main
+
+import (
+	"encoding/xml"
+	"flag"
+	"os"
+)
+
+// setLang is simple converter to set the "lang=XX" attribute for
+// each translation unit.
+type setLang struct {
+	inFile     string
+	sourceLang string
+	targetLang string
+}
+
+const _KEEP = "keep"
+
+func init() {
+	registeredConverters["set-lang"] = new(setLang)
+}
+
+func (s *setLang) Description() string {
+	return "Sets the \"lang\" attribute of all translation units in a XLIFF"
+}
+
+func (s *setLang) ParseArgs(base string, args []string) error {
+	var fs = flag.NewFlagSet(base+" set-lang", flag.ExitOnError)
+	fs.StringVar(&s.inFile, "in", "", "infile")
+	fs.StringVar(&s.targetLang, "target", _KEEP, "target language")
+	fs.StringVar(&s.sourceLang, "source", _KEEP, "source language")
+	return fs.Parse(args)
+}
+
+func (s *setLang) Convert() error {
+
+	var doc, err = xliffFromFile(s.inFile)
+	if err != nil {
+		return err
+	}
+
+	for i := range doc.File {
+
+		setOrKeep(&doc.File[i].SourceLang, s.sourceLang)
+		setOrKeep(&doc.File[i].SourceLang, s.sourceLang)
+
+		for j := range doc.File[i].Body.TransUnit {
+			setOrKeep(&doc.File[i].Body.TransUnit[j].Source.Lang, s.sourceLang)
+			setOrKeep(&doc.File[i].Body.TransUnit[j].Target.Lang, s.targetLang)
+		}
+	}
+
+	var out []byte
+	if out, err = xml.MarshalIndent(doc, "", "  "); err != nil {
+		return err
+	}
+
+	os.Stdout.WriteString(xml.Header)
+	os.Stdout.Write(out)
+
+	return nil
+}
+
+func setOrKeep(to *string, from string) {
+	if from != _KEEP {
+		*to = from
+	}
+}
